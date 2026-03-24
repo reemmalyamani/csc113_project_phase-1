bb4a0c2523ce30225674f4697cee2810f2c9be5c
jumanah27 <jal801482@gmail.com>
2026-03-24 13:38:39 +0300

    Add Employee class with constructor and calculateBill method

--- Employee.java
+++ Employee.java
@@ -1,1 +1,23 @@
+public class Employee{
 
+private String name;
+private double workHrs;
+private double salary;
+
+public Employee(String name,double workHrs,double salary){
+this.name=name;
+this.salary=salary;
+this.workHrs=workHrs;
+}
+
+public void calculateBill(Customer C){
+C.
+
+}
+
+
+
+
+
+
+}
