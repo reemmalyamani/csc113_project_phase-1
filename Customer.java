c3b6004116d37830d25f6fcea11a03594e65ba84
jumanah27 <jal801482@gmail.com>
2026-03-24 13:36:32 +0300

    Add Customer class with order handling features
    Implemented Customer class with order management and discount calculation.

--- Customer.java
+++ Customer.java
@@ -1,1 +1,67 @@
-p
+public class Customer {
+private String name;
+private String phoneNo;
+private int loyalYears;
+private int noOfOrders;
+Orders listOrders[];
+
+
+public Customer(String name,String phoneNo,int loyalYears){
+
+this.name=name;
+this.phoneNo=phoneNo;
+this.loyalYears=loyalYears;
+noOfOrders=0;
+listOrders=new Orders[10];
+}
+
+public boolean addOrder(Orders o){
+if(noOfOrders==listOrders.length)
+return false;
+
+listOrders[noOfOrders++]=new Orders(o);
+return true;
+}
+
+
+public double cusDiscount(){
+    return loyalYears*0.5;
+}
+
+
+public String toString(){
+    return name+" phoneNo: "+phoneNo+"\n number of orders is "+noOfOrders;
+}
+
+public String getName(){
+    return name;
+}
+
+
+public void displayOrders(){
+    for(int i=0;i<noOfOrders;++i){
+         if(listOrders[i]!=null){
+        System.out.println("order "+(i+1));
+        listOrders[i].display();}
+    }
+    System.out.println("*********");
+}
+
+
+public double customerTotal(){
+    double sumTotalOrders=0;
+    for(int i=0;i<noOfOrders;++i){
+        System.out.println("Total price for order "+(i+1)+"is:"+(listOrders[i].cakeTotalPrice()));
+        sumTotalOrders+=listOrders[i].cakeTotalPrice();
+    }
+   return sumTotalOrders;
+ 
+}
+
+
+
+
+}
+
+
+
