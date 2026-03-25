274a16be4a72ad4c707a4ceb2f1414665b33a56d
Reem Alyamani <alyamanireem@gmail.com>
2026-03-25 10:44:19 +0300

    Refactor Cake class with constructors and methods
    Added constructors, getters, setters, and discount calculation to the Cake class.

--- Cake.java
+++ Cake.java
@@ -1,27 +1,67 @@
 public abstract class Cake {
-  protected String type;
-  protected double price;
-  protected String size;
+    protected String type;
+    protected double price;
+    protected String size;
 
-  public Cake (String type, String size) { 
-    this.type= type;
-    this.size = size;
-  }
-  public Cake (Cake c) {
-    
-  }/?
-  public String getSize () {
-    return size;
-  }
-  public String getType () {
-    return type;
-  }
-  //add setters and getters
-  public double CalculatePrice () {
-    
-  } //what is the return method here 
-  public double getDiscount () {}
-  public void displayinfo (){
-    System.out.println (""); 
-  } //close display
-}//close cake
+    // Constructor
+    public Cake(String type, String size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    // Copy Constructor
+    public Cake(Cake c) {
+        this.type = c.type;
+        this.size = c.size;
+        this.price = c.price;
+    }
+
+    // Getters
+    public String getSize() {
+        return size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Abstract method then subclasses MUST implement
+    public abstract double CalculatePrice();
+
+    // Discount method
+    public double getDiscount(int loyalYears) {
+        if (loyalYears >= 5) {
+            return price * 0.10; // 10% discount
+        }
+        return 0;
+    }
+
+    // Display info
+    public void displayInfo(int loyalYears) {
+        double discount = getDiscount(loyalYears);
+        double finalPrice = price - discount;
+
+        System.out.println("Type: " + type);
+        System.out.println("Size: " + size);
+        System.out.println("Original Price: " + price);
+        System.out.println("Discount: " + discount);
+        System.out.println("Final Price: " + finalPrice);
+    }
+}
