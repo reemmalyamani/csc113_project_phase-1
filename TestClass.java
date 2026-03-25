df6be09b2213b5ec449b24c7e41a10c5c94b3a9b
Reem Alyamani <alyamanireem@gmail.com>
2026-03-25 10:47:05 +0300

    Correct syntax and structure in TestClass main method
    Fix syntax errors and improve structure in main method.

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,15 @@ public class TestClass {
 public static void main (String [] args ) {
 Bakery b1 = new Bakery ;
   b1.welcoming ();
-  
+  do:
+    Sysem.out.println ("choose bla bla bla ");
+
+
+    case 3: 
+    // call class based on choice
+    //
+    while (choice !=0) 
+// create employee object
+    Employee e1 = new Employee ();
 } //close main 
 }//close test class 
