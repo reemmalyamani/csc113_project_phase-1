ff26f83d2d07a8e634bdba1913d5ce6da9f24df7
jumanah27 <jal801482@gmail.com>
2026-03-26 16:58:37 +0300

    Refactor redVelvet class and method names

--- redVelvet.java
+++ redVelvet.java
@@ -1,10 +1,10 @@
 public class redVelvet extends Chocolate {
   private String dye;
-  public RedVelvet(String type, String size, int layers, boolean extra, String dye) {
+  public redVelvet(String type, String size, int layers, boolean extra, String dye) {
         super(type, size, layers, extra);
         this.dye = dye;
     }
-  public RedVelvet(RedVelvet r) {
+  public redVelvet(redVelvet r) {
         super(r);  // calls Chocolate copy constructor
         this.dye = r.dye;
     }
@@ -22,15 +22,21 @@ public class redVelvet extends Chocolate {
         return extraCost;
     }
 
-    @Override
-    public double calculatePrice() {
-        double basePrice = super.calculatePrice();
-        basePrice += addDyeCost();
-        return basePrice;
+    
+    public double CalculatePrice() {
+         this.price = super.CalculatePrice();
+        this.price += addDyeCost();
+        return price;
     }
 
-    @Override
-    public String toString() {
-        return super.toString() +
-               "\nDye: " + dye; }
-}//close red velvet
+     public void displayInfo() {
+        double discount = getDiscount();
+        double finalPrice = price - discount;
+
+        System.out.println("Type: " + type+"--- Size:"+ size+"dye:"+dye+"\nOriginal Price: " + price+"---Discount:" + discount+"Final Price: " + finalPrice);
+
+    
+       
+    }
+}
+//close red velvet
