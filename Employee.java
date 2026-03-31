b1a7a965076ceb1020f79b3bb1ff4e849682b93a
jumanah27 <jal801482@gmail.com>
2026-04-01 00:01:18 +0300

    Refactor calculateBill and add toString method
    Enhanced the calculateBill method to improve output formatting and added a toString method for better employee representation.

--- Employee.java
+++ Employee.java
@@ -1,4 +1,3 @@
-
 public class Employee {
    
 
@@ -13,14 +12,21 @@ this.workHrs=workHrs;
 }
 
 public void calculateBill(Customer C){
-System.out.println(C.getName());
+System.out.println("\nCustomer Bill");
+System.out.println(C.toString());
 
 C.displayOrders();
 
 System.out.println("\nTotal Price: "+ C.customerTotal()+"SR");
 System.out.println("Total price after customer discount:"+(C.customerTotal()-C.getDiscount()));
-System.out.println("thank you for ordering ");
-
+System.out.println("\nthank you for ordering ");
+System.out.println(toString());
 }
+
+public String toString(){
+return "responsible employee:"+name;
+}
+
+
 }
 
