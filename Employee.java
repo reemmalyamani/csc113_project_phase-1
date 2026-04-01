abfbfb424dc5cff17590720429a1235ab94d017b
jumanah27 <jal801482@gmail.com>
2026-04-01 12:36:31 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -19,12 +19,12 @@ C.displayOrders();
 
 System.out.println("\nTotal Price: "+ C.customerTotal()+"SR");
 System.out.println("Total price after customer discount: "+(C.customerTotal()-C.getDiscount()));
-System.out.println("\nthank you for ordering ");
+System.out.println("\nThank you for ordering !!");
 System.out.println(toString());
 }
 
 public String toString(){
-return "responsible employee:"+name;
+return "Responsible employee: "+name;
 }
 
 
