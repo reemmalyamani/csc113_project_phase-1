1810dc60eb69d7eed3443b0e47f05f3e810496a3
Reem Alyamani <alyamanireem@gmail.com>
2026-05-07 09:43:27 +0300

    Extend UserDefinedExample from RuntimeException

--- UserDefinedExample.java
+++ UserDefinedExample.java
@@ -1,4 +1,4 @@
-public class UserDefinedExample {
+public class UserDefinedExample extends RuntimeException  {
     public static void checkCakeType(String type) throws InvalidCakeTypeException {
         if (type == null) {
             throw new InvalidCakeTypeException("User-defined exception: cake type cannot be empty.");
